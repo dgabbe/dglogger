76ef1c86acb84512654a8967e85771ab2d65ee91
David G <dgabbe@acm.org>
2019-08-05 20:36:14 -0400

    Added missing log functions. Incorporated some of @eric-s-s thoughts.

--- dglogger.py
+++ dglogger.py
@@ -5,68 +5,84 @@
 # * https://docs.python.org/3/howto/logging.html#a-simple-example
 
 import logging
-from os import environ, getlogin, path
+from os import getlogin, path
 import platform
+from pwd import getpwnam
+from sys import stderr
 
-
+# Can this be called from __init__.py? So it's running before main()...
 def log_config():
     """Unique name by machine and user.
     OS X: ~/Library/Logs/<machine-name>_<username>.log
 
-    :param home: $HOME directory for user running process
     :return: log file name
     """
-    home = environ["HOME"]
-    machine_user = str.split(platform.uname()[1], ".local")[0] \
-        + "_" + getlogin()
-    try:
-        log_file_path = path.join(home, "Library/Logs/")
-    except:
-        log_file_path = home
-    finally:
-        log_file = path.join(log_file_path, machine_user + ".log")
+    home = getpwnam(getlogin()).pw_dir
+    machine_user = str.split(platform.uname()[1], ".local")[0] + "_" + getlogin()
+
+    log_file_path = path.join(home, "Library/Logs/")  # MacOS location
+    if not path.exists(log_file_path):
+        log_file_path = home  # Linux location
+
+    log_file = path.join(log_file_path, machine_user + ".log")
+
+    # basicConfig below will raise an exception - fix that
 
     logging.basicConfig(
-      filename=log_file,
-      format='%(asctime)s %(filename)s %(levelname)s: %(message)s',
-      datefmt='%m/%d/%Y %H:%M:%S',
-      level=logging.INFO
+        filename=log_file,
+        format="%(asctime)s %(filename)s %(levelname)s: %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
     )
     return log_file
 
 
-def log_end(f):
-  logging.info(": End")
-  logging.shutdown()
-  print("Actions recorded in ", f, ".", sep = "")
+def log_critical(msg, indent="\n    "):
+    print(indent + msg, sep="", file=stderr)
+    logging.critical(msg)
 
 
-def log_error(msg, indent = '\n    '):
-  print(indent + msg, sep = "")
-  logging.error(msg)
+def log_debug(msg, indent="\n    "):
+    print(indent + msg, sep="", file=stderr)
+    logging.debug(msg)
 
 
-def log_info(msg, indent = '\n    '):
-  print(indent + msg, sep = "")
-  logging.info(msg)
+def log_end():
+    # https://stackoverflow.com/questions/26017073/how-to-get-filename-from-a-python-logger
+    f = logging.getLogger().handlers[0].baseFilename
+    logging.info(": End")
+    logging.shutdown()
+    print("Actions recorded in ", f, ".", sep="", file=stderr)
+
+
+def log_error(msg, indent="\n    "):
+    print(indent + msg, sep="", file=stderr)
+    logging.error(msg)
+
+
+def log_info(msg, indent="\n    "):
+    print(indent + msg, sep="", file=stderr)
+    logging.info(msg)
+
 
 # path.abspath(__file__) or figure out who's calling dglogger
 def log_start():
-  logging.info(": Start")
+    logging.info(": Start")
 
 
-def log_warning(msg, indent = '\n    '):
-  print(indent + msg, sep = "")
-  logging.warning(msg)
+def log_warning(msg, indent="\n    "):
+    print(indent + msg, sep="", file=stderr)
+    logging.warning(msg)
 
 
 #
 # Test
+# Figure out Python unit testing
 #
 def test():
-    log_file = log_config()
+    log_config()
     log_start()
-    log_end(log_file)
+    log_end()
 
 
 # Revisit if this code will work on python (v2) as well

061d3e16f1da50ef8da099a424f7f5dfacc29e40
David G <dgabbe@acm.org>
2019-08-15 21:30:54 -0400

    First working version of code to write to log file and display to stderr.

--- dglogger.py
+++ dglogger.py
@@ -4,80 +4,113 @@
 # * http://inventwithpython.com/blog/2012/04/06/stop-using-print-for-debugging-a-5-minute-quickstart-guide-to-pythons-logging-module/
 # * https://docs.python.org/3/howto/logging.html#a-simple-example
 # * Eric's code ideas: https://gist.github.com/eric-s-s/11e08dbc38891fa3d0eba5396703afc6
-
-import logging
+from functools import partial
+from logging import (
+    basicConfig,
+    critical,
+    debug,
+    error,
+    exception,
+    Formatter,
+    getLogger,
+    INFO,
+    info,
+    Logger,
+    shutdown,
+    StreamHandler,
+    warning,
+)
 from os import getlogin, path
 import platform
 from pwd import getpwnam
 from sys import stderr
 
+
+# Conditional formatting possible based on levelname?
+# change time to exclude milliseconds
+def get_stream_handler() -> StreamHandler:
+    # By default goes to stderr
+    console_format = "    %(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s"
+    # console_format = "    %(Y)s-%(m)s %(levelname)s %(module)s:%(lineno)d %(message)s"
+    stream_formatter = Formatter(fmt=console_format)
+    sh = StreamHandler()
+    sh.setFormatter(stream_formatter)
+    return sh
+
+
 # Can this be called from __init__.py? So it's running before main()...
-def log_config():
+def log_config(is_log_file_required=False) -> Logger:
     """Unique name by machine and user.
     OS X: ~/Library/Logs/<machine-name>_<username>.log
 
-    :return: log file name
+    References:
+    - https://docs.python.org/3/library/logging.html
+    - https://docs.python.org/3/howto/logging-cookbook.html#logging-to-multiple-destinations
+
+    :return: Logger object
     """
 
-    # https://docs.python.org/3/howto/logging-cookbook.html#logging-to-multiple-destinations to use instead
-    # of print to stderr.
+    user = getlogin()
+    home_dir = getpwnam(user).pw_dir
+    machine_user = str.split(platform.uname()[1], ".local")[0] + "_" + user
 
-    home = getpwnam(getlogin()).pw_dir
-    machine_user = str.split(platform.uname()[1], ".local")[0] + "_" + getlogin()
-
-    log_file_path = path.join(home, "Library/Logs/")  # MacOS location
+    log_file_path = path.join(home_dir, "Library/Logs/")  # MacOS location
     if not path.exists(log_file_path):
-        log_file_path = home  # Linux location
+        log_file_path = home_dir  # Linux location
 
-    log_file = path.join(log_file_path, machine_user + ".log")
+    log_file_name = path.join(log_file_path, machine_user + ".log")
 
-    # basicConfig below will raise an exception - fix that
+    try:
+        basicConfig(
+            filename=log_file_name,
+            format="%(asctime)s %(filename)s %(levelname)s: %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+            level=INFO,
+        )
+    except (PermissionError, FileNotFoundError):
+        exception(f"Could not create log file at: {log_file_name}")
+        # Add check for is_required...
 
-    logging.basicConfig(
-        filename=log_file,
-        format="%(asctime)s %(filename)s %(levelname)s: %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        level=logging.INFO,
-    )
-    return log_file
+    logger = getLogger()
+    console_handler = get_stream_handler()
+    logger.addHandler(console_handler)
+    return logger
 
 
-def log_critical(msg: str, indent: str = "\n    "):
-    print(indent + msg, sep="", file=stderr)
-    logging.critical(msg)
+def log_critical(msg: str):
+    critical(msg)
 
 
-def log_debug(msg: str, indent: str = "\n    "):
-    print(indent + msg, sep="", file=stderr)
-    logging.debug(msg)
+def log_debug(msg: str):
+    debug(msg)
 
 
 def log_end():
     # https://stackoverflow.com/questions/26017073/how-to-get-filename-from-a-python-logger
-    f = logging.getLogger().handlers[0].baseFilename
-    logging.info(": End")
-    logging.shutdown()
-    print("Actions recorded in ", f, ".", sep="", file=stderr)
+    f = getLogger().handlers[0].baseFilename
+    info("End")
+    shutdown()
+    print_err("Actions recorded in", f)
 
 
-def log_error(msg: str, indent: str = "\n    "):
-    print(indent + msg, sep="", file=stderr)
-    logging.error(msg)
+def log_error(msg: str):
+    error(msg)
 
 
-def log_info(msg: str, indent: str = "\n    "):
-    print(indent + msg, sep="", file=stderr)
-    logging.info(msg)
+def log_info(msg: str):
+    info(msg)
 
 
 # path.abspath(__file__) or figure out who's calling dglogger, think there is a feature for that
 def log_start():
-    logging.info(": Start")
+    info("Start")
 
 
-def log_warning(msg: str, indent: str = "\n    "):
-    print(indent + msg, sep="", file=stderr)
-    logging.warning(msg)
+def log_warning(msg: str):
+    warning(msg)
+
+
+print_err = partial(print, file=stderr)
 
 
 #
@@ -91,4 +124,5 @@ def test():
     log_warning("Testing log_warning()")
     log_error("Testing log_error()")
     log_debug("Testing log_debug()")
+    log_critical("Testing log_critical()")
     log_end()

95f6eee03154f5c5e7283b8664869dbebc26d0b5
David G <dgabbe@acm.org>
2019-09-24 06:07:30 -0400

    Bump version # for pip install.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dglogger",
-    version="0.0.1",
+    version="0.0.2",
     author="David G.",
     author_email="dgabbe@acm.org",
     description="Logging utility for development & production",

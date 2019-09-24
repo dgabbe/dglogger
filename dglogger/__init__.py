cdaac49c2caca768ed07555bb79b83726136154c
David G <dgabbe@acm.org>
2019-09-24 06:00:14 -0400

    Declare explicit exports.

--- dglogger/__init__.py
+++ dglogger/__init__.py
@@ -1,1 +1,15 @@
-name = "dglogger" # not used by PyPi
+name = "dglogger"  # For PyPi, still used?
+__name__ = "dglogger"
+__package__ = "dglogger"
+
+from dglogger.dglogger import (
+    log_config,
+    log_critical,
+    log_debug,
+    log_dev,
+    log_end,
+    log_error,
+    log_info,
+    log_start,
+    log_warning,
+)
